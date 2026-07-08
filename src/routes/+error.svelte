ef735c5f459e5da21668302b6df7751f30de8ea9
Wendall Cada <wendallc@83864.com>
2026-07-08 16:25:14 -0700

    Replace ModeWatcher with custom theme switcher.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,14 +2,13 @@
     import { page } from '$app/state';
 </script>
 
-<main class="mx-4 my-32 flex max-w-5xl flex-col items-center gap-16 self-center">
+<main>
     <article class="mb-8 flex flex-col gap-16">
         <div class="flex flex-col items-center gap-4 self-center">
             <h1 class="pb-1 text-5xl">Error {page.status}</h1>
             <span class="text-lg">{page.error?.message ?? 'Page Not Found'}</span>
         </div>
 
-        <!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
-        <a href="/"> Home </a>
+        <a href="/" data-sveltekit-reload> Home </a>
     </article>
 </main>
